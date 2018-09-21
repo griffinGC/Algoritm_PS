7ea70b7aeebc08629bca474000bb2c402100f307
griffinGC <32213721+griffinGC@users.noreply.github.com>
2018-09-22 01:31:52 +0900

    Update 2750.java
    bubbleSort

--- 2750.java
+++ 2750.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class insertionSort {
+public class bubbleSort {
 	public static void main(String[] args) throws Exception{
 		InputStreamReader isr = new InputStreamReader(System.in);
 		BufferedReader bsr = new BufferedReader(isr);
