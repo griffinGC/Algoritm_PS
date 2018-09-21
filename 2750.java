9e771920ccdd5e3d973d9a48318f1fa59f45ebea
griffinGC <32213721+griffinGC@users.noreply.github.com>
2018-09-22 01:27:13 +0900

    Update 2750.java
    insertion_sorting

--- 2750.java
+++ 2750.java
@@ -1,19 +1,28 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-public class Main {
-	public static void main(String[] args) throws Exception {
+
+public class insertionSort {
+	public static void main(String[] args) throws Exception{
 		InputStreamReader isr = new InputStreamReader(System.in);
-		BufferedReader bufread = new BufferedReader(isr);
-		int n = Integer.parseInt(bufread.readLine());
-		int[] count = new int[n];
-		for(int i =0; i<n; i++) {
-			count[i] = Integer.parseInt(bufread.readLine());
+		BufferedReader bsr = new BufferedReader(isr);
+		
+		int n = Integer.parseInt(bsr.readLine());
+		int number[] = new int[n];
+		for(int i = 0; i<n; i++) {
+			number[i] = Integer.parseInt(bsr.readLine());
 		}
-		Arrays.sort(count);
-		for(int i= 0; i<count.length; i++) {
-			System.out.println(count[i]);			
+		for(int i = 0; i<n-1; i++) {
+			for(int j = i+1; j<n; j++) {
+				if(number[i] > number[j]) {
+					int temp = number[i];
+					number[i] = number[j];
+					number[j] = temp;
+				}
+			}
+		}
+		
+		for(int i = 0; i<n; i++) {
+			System.out.println(number[i]);
 		}
 	}
 }
