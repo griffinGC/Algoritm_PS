60866ecef2dfd212cd64797d480c3c98b0c6c827
griffinGC <32213721+griffinGC@users.noreply.github.com>
2019-04-03 22:18:52 +0900

    Create 2348_printStar1.java

---
+++ 2348_printStar1.java
@@ -0,0 +1,16 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class boj_2348_starPrint1 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		for(int i = 1; i<=n; i++) {
+			for(int j = 1; j<=i ;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
